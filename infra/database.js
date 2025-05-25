ddce209892454511f52ec95f55ac5e099af3558a
vdaroz <76072825+vdaroz@users.noreply.github.com>
2025-05-25 20:01:47 +0000

    Handles SSL connection to database

--- infra/database.js
+++ infra/database.js
@@ -6,20 +6,22 @@ async function query(queryObject){
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV == "development" ? false : true 
   });
-  console.log("Credenciais Postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
-  });
+  
   try{
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   }catch(err){
+    console.log("Credenciais Postgres:", {
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD
+    });
     console.error(err.message);
     throw err;
   }finally{
